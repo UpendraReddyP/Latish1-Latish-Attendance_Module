196d6e133ad4a4ca9d5670a976c3321caa8c2cc3
UpendraReddyP <upendrareddyp7@gmail.com>
2025-07-15 20:16:53 +0530

    updated

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS punch_records (
     id SERIAL PRIMARY KEY,
     employee_id VARCHAR(50) NOT NULL,
-    status VARCHAR(10) NOT NULL, -- 'in' or 'out'
+    status VARCHAR(10) NOT NULL,
     timestamp TIME NOT NULL,
     date DATE NOT NULL,
     hours_worked NUMERIC(5,2),
